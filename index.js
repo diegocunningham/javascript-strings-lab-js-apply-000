66cca9ae10fec0798d34d244cc96334f10f5a262
candescent-minifier-5282 <cunninghamdiego@yahoo.com>
2019-01-14 18:43:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var greeting = "Hello Everybody!";
 var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = " + specialGuest + !";
-
+var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
